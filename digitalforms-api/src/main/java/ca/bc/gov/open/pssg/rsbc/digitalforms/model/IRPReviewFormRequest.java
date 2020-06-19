5e10a09fbf50f87038b027e2fc945e898587bb6a
Siva Karunakaran <siva.karunakaran@gov.bc.ca>
2020-06-19 08:58:21 -0700

    Added todo comment

--- digitalforms-api/src/main/java/ca/bc/gov/open/pssg/rsbc/digitalforms/model/IRPReviewFormRequest.java
+++ digitalforms-api/src/main/java/ca/bc/gov/open/pssg/rsbc/digitalforms/model/IRPReviewFormRequest.java
@@ -13,7 +13,8 @@ import com.fasterxml.jackson.annotation.JsonProperty;
 
 @JsonInclude(JsonInclude.Include.NON_NULL)
 public class IRPReviewFormRequest {
-	
+
+	// TODO Update dummy value
 	@JsonProperty("data")
 	private String data;
 
