6431d46d45f23d86678170ca980ac6904e70c139
Shaun Millar <shaun.millar@nttdata.com>
2020-06-25 14:13:09 -0700

    DF-217_B1
    Added payment controller and supporting service.

---
+++ digitalforms-api/src/main/java/ca/bc/gov/open/pssg/rsbc/digitalforms/model/TransactionInfo.java
@@ -0,0 +1,47 @@
+package ca.bc.gov.open.pssg.rsbc.digitalforms.model;
+
+import com.fasterxml.jackson.annotation.JsonInclude;
+import com.fasterxml.jackson.annotation.JsonProperty;
+import com.fasterxml.jackson.annotation.JsonPropertyOrder;
+
+/**
+ * 
+ * Supporting object for the IRPPaymentTransRequest. 
+ * 
+ * TODO - Needs refining once the business determines what needs to be registered in VIPS after payment received. 
+ * 
+ * @author shaunmillargov
+ *
+ */
+@JsonInclude(JsonInclude.Include.NON_NULL)
+@JsonPropertyOrder({ "totalPrice", "cardType" })
+public class TransactionInfo {
+
+	@JsonProperty("totalPrice")
+	private String totalPrice;
+	
+	@JsonProperty("cardType")
+	private String cardType;
+
+
+	@JsonProperty("totalPrice")
+	public String getTotalPrice() {
+		return totalPrice;
+	}
+
+	@JsonProperty("totalPrice")
+	public void setTotalPrice(String totalPrice) {
+		this.totalPrice = totalPrice;
+	}
+
+	@JsonProperty("cardType")
+	public String getCardType() {
+		return cardType;
+	}
+
+	@JsonProperty("cardType")
+	public void setCardType(String cardType) {
+		this.cardType = cardType;
+	}
+
+}
