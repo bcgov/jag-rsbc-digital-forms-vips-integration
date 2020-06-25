3f3faade3c83cc42ee2979317d3ac9cc5dccc2e0
Siva Karunakaran <siva.karunakaran@gov.bc.ca>
2020-06-25 11:18:18 -0700

    Added swagger annotations and fixed unit test run error due to swagger

--- digitalforms-api/src/test/java/ca/bc/gov/open/pssg/rsbc/digitalforms/DigitalFormsApplicationTests.java
+++ digitalforms-api/src/test/java/ca/bc/gov/open/pssg/rsbc/digitalforms/DigitalFormsApplicationTests.java
@@ -2,8 +2,10 @@ package ca.bc.gov.open.pssg.rsbc.digitalforms;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.TestPropertySource;
 
 @SpringBootTest
+@TestPropertySource("classpath:application-test.properties")
 class DigitalFormsStarterApplicationTests {
 
 	@Test
