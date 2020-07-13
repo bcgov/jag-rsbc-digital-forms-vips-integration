890c4b5659d38e889cc8c5bcaf86f712bd92cf07
Shaun Millar <shaun.millar@nttdata.com>
2020-07-13 14:09:20 -0700

    Update PaymentServiceController.java
    Removed /IRP/ in path for Payment service controller.

--- digitalforms-api/src/main/java/ca/bc/gov/open/pssg/rsbc/digitalforms/controller/PaymentServiceController.java
+++ digitalforms-api/src/main/java/ca/bc/gov/open/pssg/rsbc/digitalforms/controller/PaymentServiceController.java
@@ -6,7 +6,6 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import ca.bc.gov.open.pssg.rsbc.digitalforms.model.JSONResponse;
@@ -26,7 +25,6 @@ import io.swagger.annotations.ApiResponses;
  *
  */
 @RestController
-@RequestMapping("/IRP/")
 @Api(value = "Payment", tags = { "Payment" })
 public class PaymentServiceController {
 	
