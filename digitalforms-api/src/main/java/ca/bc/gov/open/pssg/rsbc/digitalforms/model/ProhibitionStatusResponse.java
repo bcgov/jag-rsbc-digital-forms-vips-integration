0662895179ea35ce4e483a9568a9301fb5494d45
Shaun Millar <shaun.millar@nttdata.com>
2020-07-21 14:49:06 -0700

    DF-267_3
    Initial updates supporting prohibition status updates.

---
+++ digitalforms-api/src/main/java/ca/bc/gov/open/pssg/rsbc/digitalforms/model/ProhibitionStatusResponse.java
@@ -0,0 +1,45 @@
+package ca.bc.gov.open.pssg.rsbc.digitalforms.model;
+
+import com.fasterxml.jackson.annotation.JsonInclude;
+import com.fasterxml.jackson.annotation.JsonProperty;
+import com.fasterxml.jackson.annotation.JsonPropertyOrder;
+
+import ca.bc.gov.open.jagvipsclient.prohibition.VipsProhibitionStatusResponse;
+
+/**
+ * 
+ * @author shaunmillargov
+ *
+ */
+
+@JsonInclude(JsonInclude.Include.NON_NULL)
+@JsonPropertyOrder({ "status" })
+public class ProhibitionStatusResponse {
+
+	@JsonProperty("status")
+	private Status status;
+
+	public ProhibitionStatusResponse(VipsProhibitionStatusResponse ordsResp) {
+		status = new Status();
+		status.setEffectiveDt(ordsResp.getStatus().getEffectiveDt());
+		status.setNoticeTypeCd(ordsResp.getStatus().getNoticeTypeCd());
+		status.setOriginalCause(ordsResp.getStatus().getOriginalCause());
+		status.setReceiptNumberTxt(ordsResp.getStatus().getReceiptNumberTxt());
+		status.setReviewCreatedYn(ordsResp.getStatus().getReviewCreatedYn());
+		status.setReviewEndDtm(ordsResp.getStatus().getReviewEndDtm());
+		status.setReviewFormSubmittedYn(ordsResp.getStatus().getReviewFormSubmittedYn());
+		status.setReviewStartDtm(ordsResp.getStatus().getReviewStartDtm());
+		status.setSurnameNm(ordsResp.getStatus().getSurnameNm());
+	}
+
+	@JsonProperty("status")
+	public Status getStatus() {
+		return status;
+	}
+
+	@JsonProperty("status")
+	public void setStatus(Status status) {
+		this.status = status;
+	}
+
+}
