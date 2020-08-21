0ad236eb6e66ca3b51e65b0c9bbfabd6fa0bd768
Ebenezer Muthiah <ebe1234@gmail.com>
2020-08-21 16:18:25 -0700

    Adding BDT pipeline and configuration * Updating documentaion * Updating env.template, environment variables to use camel case, Linux doesn't allow "-" in variable names * Updating GetConfiguration.java to reflect new environment variables * Adding static-web-server deployment configuration for test reports * Adding cucumber-maven-config, environment variables for mvn test

--- test-automation/src/test/java/config/GetConfiguration.java
+++ test-automation/src/test/java/config/GetConfiguration.java
@@ -16,36 +16,39 @@ public class GetConfiguration {
 
     public String getDigitalFormsUrl()
     {
-        return System.getenv("digitalFormsDevBaseUrl");
+        return System.getenv("digitalFormsBaseUrl");
     }
     public String getApiUsername()
     {
-        return System.getenv("digitalFormsApi-username");
+        return System.getenv("digitalFormsApiUsername");
     }
     public String getApiPassword()
     {
-        return System.getenv("digitalFormsApi-password");
+        return System.getenv("digitalFormsApiPassword");
     }
     public String getDbUsername()
     {
-        return System.getenv("db-dev-username");
+        return System.getenv("dbUsername");
     }
     public String getDbPassword()
     {
-        return System.getenv("db-dev-password");
+        return System.getenv("dbPassword");
     }
     public String getTestEnvironment()
     {
-        return System.getenv("automation-env");
+        return System.getenv("automationEnv");
     }
-    public String getConnectionString(){ return System.getenv("db-dev-connectionString");}
+    public String getConnectionString()
+    {
+        return System.getenv("dbConnectionString");
+    }
     public String getOrdsApiUsername()
     {
-        return System.getenv("OrdsApi-username");
+        return System.getenv("ordsApiUsername");
     }
     public String getOrdsApiPassword()
     {
-        return System.getenv("OrdsApi-password");
+        return System.getenv("ordsApiPassword");
     }
 
 }
