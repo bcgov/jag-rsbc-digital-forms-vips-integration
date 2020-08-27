d7be43866b41f07461171a87fea7b74b82fd7920
Sirohi <208724@NTTDATA.COM>
2020-08-26 20:43:15 -0700

    Removed database connection and added ords delete endpoint

--- test-automation/src/test/java/stepdefs/PatchPaymentServiceController.java
+++ test-automation/src/test/java/stepdefs/PatchPaymentServiceController.java
@@ -1,10 +1,9 @@
 package stepdefs;
 
+import api.ApiUtil;
 import api.GenerateEndpoint;
 import api.VerbType;
-import base.BaseUtil;
 import config.GlobalVariables;
-import database.DatabaseConnection;
 import io.cucumber.java.en.And;
 import io.cucumber.java.en.Given;
 import io.cucumber.java.en.Then;
@@ -12,8 +11,7 @@ import io.cucumber.java.en.When;
 import org.testng.Assert;
 import org.testng.asserts.SoftAssert;
 
-import java.sql.ResultSet;
-import java.sql.SQLException;
+import java.util.HashMap;
 
 import static io.restassured.RestAssured.given;
 import static io.restassured.http.ContentType.JSON;
@@ -21,30 +19,31 @@ import static io.restassured.http.ContentType.JSON;
 public class PatchPaymentServiceController {
 
     GenerateEndpoint generateEndpoint;
-    DatabaseConnection databaseConnection;
-    BaseUtil baseUtil;
+    ApiUtil apiUtil;
+    HashMap<String, HashMap<String, String>> jsonData;
+    HashMap<String, String> data;
     SoftAssert softAssert;
-    String selectQuery, endpointUrl;
     int responseStatusCode;
-    String guid;
+    String endpointUrl;
+    String noticeNumber, correlationId;
     String paymentAmount, paymentCardType, paymentDate, receiptNumberTxt;
     String paymentAmount_updated, paymentCardType_updated, paymentDate_updated, receiptNumberTxt_updated;
 
-    public PatchPaymentServiceController(BaseUtil baseUtil) {
+    public PatchPaymentServiceController() {
         generateEndpoint = new GenerateEndpoint(GlobalVariables.apiBaseUrl);
-        this.baseUtil = baseUtil;
-        this.databaseConnection = baseUtil.databaseConnection;
         softAssert = new SoftAssert();
     }
 
-    @Given("the database has a record for FormType {string} GUID {string} CorrelationId {string}")
-    public void theDatabaseHasARecordForFormTypeGUIDCorrelationId(String formType, String guid, String correlationId) throws Exception {
-        selectQuery = "select * from rsdfrm_digital_forms where notice_type_cd='" + formType + "' and correlation_guid='" + correlationId + "' and form_object_guid='" + guid + "'";
-
-        this.guid = guid;
-        int rowCount = databaseConnection.getRowCount(selectQuery);
-        Assert.assertEquals(rowCount, 1, "There should be ONE record in the database for SQL Query " + selectQuery);
+    @Given("the database has a record for NoticeNumber {string} and CorrelationId {string}")
+    public void theDatabaseHasARecordForNoticeNumberAndCorrelationId(String noticeNumber, String correlationId) {
+        endpointUrl = generateEndpoint.Payment(VerbType.GET, noticeNumber, correlationId);
+        apiUtil = new ApiUtil(endpointUrl);
+        responseStatusCode = apiUtil.getResponseStatusCodeUsingBasicAuthentication(GlobalVariables.apiUsername, GlobalVariables.apiPassword);
+        Assert.assertEquals(responseStatusCode, 200, "The record to be updated doesn't exist in database.");
+        this.noticeNumber = noticeNumber;
+        this.correlationId = correlationId;
     }
+
 
     @When("the user submits a PATCH Payment request with parameters NoticeNumber {string} Correlation ID {string}")
     public void theUserSubmitsAPATCHPaymentRequestWithParametersNoticeNumberCorrelationID(String noticeNumber, String correlationId) {
@@ -60,8 +59,8 @@ public class PatchPaymentServiceController {
 
         paymentAmount_updated = "9999.99";
         paymentCardType_updated = "MASTER CARD";
-        paymentDate_updated = "2020-01-01";
-        receiptNumberTxt_updated = "stepdefs.PatchApplicationFormData.test";
+        paymentDate_updated = "2020-01-01 00:00:00 -07:00";
+        receiptNumberTxt_updated = receiptNumberTxt + "_updated";
 
         String jsonBody = "{\n" +
                 "    \"transactionInfo\": {\n" +
@@ -77,27 +76,38 @@ public class PatchPaymentServiceController {
     }
 
     @Then("the user should get a successful response from PATCH Payment request and the record should be updated in the database")
-    public void theUserShouldGetASuccessfulResponseFromPATCHPaymentRequestAndTheRecordShouldBeUpdatedInTheDatabase() throws Exception {
+    public void theUserShouldGetASuccessfulResponseFromPATCHPaymentRequestAndTheRecordShouldBeUpdatedInTheDatabase() {
         Assert.assertEquals(responseStatusCode, 200, "The response status should be OK:200");
-        selectQuery = "select * from rsdfrm_digital_forms where form_object_guid='" + guid + "'";
 
-        int rowCount = databaseConnection.getRowCount(selectQuery);
-        Assert.assertEquals(rowCount, 1, "There should be ONE record in the database for SQL Query " + selectQuery);
+        endpointUrl = generateEndpoint.Payment(VerbType.GET, noticeNumber, correlationId);
+        apiUtil = new ApiUtil(endpointUrl);
+        responseStatusCode = apiUtil.getResponseStatusCodeUsingBasicAuthentication(GlobalVariables.apiUsername, GlobalVariables.apiPassword);
+        Assert.assertEquals(responseStatusCode, 200, "Failed to update the database record using PATCH request.");
 
-        ResultSet dbResult = databaseConnection.executeSelectQuery(selectQuery);
-        dbResult.next();
+        jsonData = apiUtil.getAttributeValuesInNestedHashMap("data");
+        data = jsonData.get("transactionInfo");
 
-        softAssert.assertEquals(dbResult.getString("payment_amt"),paymentAmount_updated,"Payment Amount mismatch.");
-        softAssert.assertEquals(dbResult.getString("payment_card_type_txt"),paymentCardType_updated,"Payment Card Type mismatch.");
-        softAssert.assertEquals(dbResult.getString("receipt_number_txt"),receiptNumberTxt_updated,"Receipt Number Text mismatch.");
-        //softAssert.assertEquals(dbResult.getString("payment_dtm"),paymentDate_updated,"Payment Date mismatch.");
+        softAssert.assertEquals(data.get("paymentAmount"),paymentAmount_updated,"Payment Amount mismatch.");
+        softAssert.assertEquals(data.get("paymentCardType"),paymentCardType_updated,"Payment Card Type mismatch.");
+        softAssert.assertEquals(data.get("receiptNumberTxt"),receiptNumberTxt_updated,"Receipt Number Text mismatch.");
+//        softAssert.assertEquals(data.get("paymentDate"),paymentDate_updated,"Payment Date mismatch.");
         softAssert.assertAll();
     }
 
     @And("the Payment db record should be reverted to the original state")
-    public void thePaymentDbRecordShouldBeRevertedToTheOriginalState() throws SQLException {
-        String updateSqlQuery = "update rsdfrm_digital_forms set payment_amt='"+paymentAmount+"', payment_card_type_txt ='"+paymentCardType+"', payment_dtm='"+paymentDate+"', receipt_number_txt='"+receiptNumberTxt+"' where form_object_guid='" + guid + "'";
-        int updatedRows = databaseConnection.executeUpdateQuery(updateSqlQuery);
-        Assert.assertEquals(updatedRows, 1, "One row should be updated for Query " + updateSqlQuery);
+    public void thePaymentDbRecordShouldBeRevertedToTheOriginalState() {
+        endpointUrl = generateEndpoint.Payment(VerbType.PATCH,noticeNumber,correlationId);
+        String jsonBody = "{\n" +
+                "    \"transactionInfo\": {\n" +
+                "        \"paymentAmount\": \""+paymentAmount+"\",\n" +
+                "        \"paymentCardType\": \""+paymentCardType+"\",\n" +
+                "        \"paymentDate\": \""+paymentDate+"\",\n" +
+                "        \"receiptNumberTxt\": \""+receiptNumberTxt+"\"\n" +
+                "    }\n" +
+                "}";
+
+        responseStatusCode = given().auth().preemptive().basic(GlobalVariables.apiUsername,GlobalVariables.apiPassword).contentType(JSON).baseUri(endpointUrl).body(jsonBody).patch().getStatusCode();
+        Assert.assertEquals(responseStatusCode, 200, "Failed to revert the database record using PATCH request");
     }
+
 }
