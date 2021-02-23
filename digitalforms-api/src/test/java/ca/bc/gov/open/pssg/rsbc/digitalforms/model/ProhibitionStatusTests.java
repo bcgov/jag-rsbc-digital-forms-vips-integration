3aa65ce71d9b140a064d2bcc4d5de9a3508a10e7
shaunmillargov <47906363+shaunmillargov@users.noreply.github.com>
2021-02-23 14:24:05 -0800

    DF-981_1: Initial JSON Object updates (#123)
    Note: This work needs to be updated once the ORDS response is working as expected.
    
    Co-authored-by: Shaun Millar <shaun.millar@nttdata.com>

--- digitalforms-api/src/test/java/ca/bc/gov/open/pssg/rsbc/digitalforms/model/ProhibitionStatusTests.java
+++ digitalforms-api/src/test/java/ca/bc/gov/open/pssg/rsbc/digitalforms/model/ProhibitionStatusTests.java
@@ -24,10 +24,10 @@ public class ProhibitionStatusTests {
 		status.setNoticeServedDt("2018-06-20 00:00:00 -07:00");
 		status.setNoticeTypeCd("IRP");
 		status.setOriginalCause("IRP3");
-		status.setReceiptNumberTxt("12345");
+		//status.setReceiptNumberTxt("12345");
 		status.setReviewCreatedYn("Y");
-		status.setReviewEndDtm("2018-06-29 00:00:00 -07:00");
-		status.setReviewStartDtm("2018-06-25 00:00:00 -07:00");
+		//status.setReviewEndDtm("2018-06-29 00:00:00 -07:00");
+		//status.setReviewStartDtm("2018-06-25 00:00:00 -07:00");
 		status.setReviewFormSubmittedYn("Y");
 		status.setSurnameNm("Smith"); 
 		status.setDriverLicenceSeizedYn("N");
@@ -41,16 +41,35 @@ public class ProhibitionStatusTests {
 		Assertions.assertEquals("2018-06-20 00:00:00 -07:00", status.getNoticeServedDt());
 		Assertions.assertEquals("IRP", status.getNoticeTypeCd());
 		Assertions.assertEquals("IRP3",  status.getOriginalCause());
-		Assertions.assertEquals("12345", status.getReceiptNumberTxt());
+		//Assertions.assertEquals("12345", status.getReceiptNumberTxt());
 		Assertions.assertEquals("Y", status.getReviewCreatedYn());
-		Assertions.assertEquals("2018-06-29 00:00:00 -07:00", status.getReviewEndDtm());
-		Assertions.assertEquals("2018-06-25 00:00:00 -07:00", status.getReviewStartDtm());
+		//Assertions.assertEquals("2018-06-29 00:00:00 -07:00", status.getReviewEndDtm());
+		//Assertions.assertEquals("2018-06-25 00:00:00 -07:00", status.getReviewStartDtm());
 		Assertions.assertEquals("Y", status.getReviewFormSubmittedYn());
 		Assertions.assertEquals("Smith", status.getSurnameNm());
 		Assertions.assertEquals("N", status.getDriverLicenceSeizedYn());
 		Assertions.assertEquals("123", status.getDisclosure().get(0).getDocumentId());
 		Assertions.assertEquals("2018-06-20 00:00:00 -07:00", status.getDisclosure().get(0).getDisclosedDtm());
 
+		Assertions.assertTrue(status.getReviews().isEmpty());
+		
+		ReviewInfo  revInfo = new ReviewInfo();
+		revInfo.setApplicationId("bb71037c-f87b-0444-e054-00144ff95452");
+		revInfo.setStatus("in_progress");
+		revInfo.setReviewStartDtm("2021-03-10 09:30:00 -07:00");
+		revInfo.setReviewEndDtm("2021-03-10 10:00:00 -07:00");
+		revInfo.setReceiptNumberTxt("1234");
+		revInfo.setReviewId("5676767");
+		
+		status.getReviews().add(revInfo);
+		
+		Assertions.assertEquals("bb71037c-f87b-0444-e054-00144ff95452", status.getReviews().get(0).getApplicationId());
+		Assertions.assertEquals("in_progress", status.getReviews().get(0).getStatus());
+		Assertions.assertEquals("2021-03-10 09:30:00 -07:00", status.getReviews().get(0).getReviewStartDtm());
+		Assertions.assertEquals("2021-03-10 10:00:00 -07:00", status.getReviews().get(0).getReviewEndDtm());
+		Assertions.assertEquals("1234", status.getReviews().get(0).getReceiptNumberTxt());
+		Assertions.assertEquals("5676767", status.getReviews().get(0).getReviewId());
+		
 	}
 
 }
